b7774105b80b357d8df1f6d30684dc139b9eeb1e
flowxyno <tracy.l.harvey6@gmail.com>
2023-01-22 16:24:08 -0800

    Code transfer
    HTML and CSS code from my personal page have been transferred to the react app

--- IS302/REACT-APPS/web-page-conversion/src/App.js
+++ IS302/REACT-APPS/web-page-conversion/src/App.js
@@ -3,22 +3,48 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <header>
+		  <img src="banner.jpg" alt="Image" usemap="#home" id="banner" />
+		  <map name="home">
+			  <area shape="rect" coords="10, 10, 400, 200" href="Home_Page.html" alt="Home Page" />
+		  </map>
+		  <nav class="horizontal">
+			  <ul>
+				  <li><a href="Home_Page.html">Home</a></li>
+				  <li><a href="Tracy.html">Tracy</a></li>
+				  <li><a href="Brittany.html">Brittany</a></li>
+				  <li><a href="Charlie.html">Charlie</a></li>
+				  <li><a href="Matthew.html">Matthew</a></li>
+			  </ul>
+		  </nav>
+	  </header>
+
+	  <div class="wrapper">
+		  <div >
+			  <figure class="image" >
+				  <img src="TPhoto.jpg" alt="Image" />
+				  <figcaption> Tracy Harvey </figcaption>
+			  </figure>
+		  </div>
+		  <div class="text" >
+			  <br/>
+			  <h1>Heading</h1>
+			  <br/>
+			  <p>Text.</p>
+			  <br/>
+        <br/>
+			  <h2>Heading</h2>
+			  <br/>
+			  <p>Text.</p>
+			  <br/>
+		  </div>
+	  </div>
+
+	  <footer>
+		  <a href="Contact_us.html">Contact Us</a>
+	  </footer>
+   </>
   );
 }
 
