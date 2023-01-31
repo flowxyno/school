025cfd57cdba8c8ad98a276d53cdd4881d8a0c33
flowxyno <63746813+flowxyno@users.noreply.github.com>
2023-01-31 00:11:12 -0800

    Initial code push for the skill demo

---
+++ IS302/REACT-APPS/stateful-component-demo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+
+function App() {
+  return (
+    <>
+      <NewComponent/>
+    </>
+  );
+};
+
+function NewComponent() {
+  return <>
+      Hello World !!!
+    </>
+}
+
+export default App;
