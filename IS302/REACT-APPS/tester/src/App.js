6d0f282a412ec9812248e7f61ed7aacf0f41a134
flowxyno <63746813+flowxyno@users.noreply.github.com>
2023-02-14 14:13:02 -0800

    card work in tester

--- IS302/REACT-APPS/tester/src/App.js
+++ IS302/REACT-APPS/tester/src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <>
-      
+      <Cards/>
+      <br/>
+      <Cards/>
+      <br/>
+      <Cards/>
+      <br/>
+      <Cards/>
     </>
   );
 };
+
+function Cards() {
+  return <>
+    <Card id="test">
+      This is the card content !!!!
+    </Card>
+  </>
+}
 
 export default App;
