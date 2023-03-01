a9c4e2cd655ebcd627ebb4cad810bde4343436b4
flowxyno <tracy.l.harvey6@gmail.com>
2023-03-01 00:00:28 -0800

    Lab night code in tester

--- IS302/REACT-APPS/tester/src/App.js
+++ IS302/REACT-APPS/tester/src/App.js
@@ -1,33 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
-import * as React from 'react'
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+  import './App.css';
+  import * as React from 'react';
 
-function App() {
-  return (
-    <>
-      <Cards/>
-      <br/>
-      <Cards/>
-      <br/>
-      <Cards/>
-      <br/>
-      <Cards/>
-    </>
-  );
-};
+  function App() {
+      return (
+          <>
+            <Greetings/>
+          </>
+        );
+    };
 
-function Cards() {
-  return <>
-    <Card id="test">
-      This is the card content !!!!
-    </Card>
-  </>
-}
+    function Greetings(){
+        let [fname, setFname] = React.useState("No data yet")
+        let [lname, setLname] = React.useState("No data yet")
+        let [loc, setLoc] = React.useState("No data yet")
 
-export default App;
+        React.useEffect(() => {
+            fetch('http://54.245.44.139:3000') // You will get this url from the glitch preview window
+                .then((response) => response.json())
+                .then((data) => {
+                    setFname(data[1].FirstName)
+                    setLname(data[1].LastName)
+                    setLoc(data[1].City)
+                });
+        })
+
+        return <>
+            Hello, my name is {fname} {lname} and I live in {loc}
+        </>
+    }
+
+  export default App;
