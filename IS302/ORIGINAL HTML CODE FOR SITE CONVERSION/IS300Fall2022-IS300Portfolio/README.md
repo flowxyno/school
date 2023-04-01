27b6595903426dda2bd326567f5b4668c9f48fe9
flowxyno <tracy.l.harvey6@gmail.com>
2023-03-31 17:03:29 -0700

    after quarter clean up

--- IS302/ORIGINAL HTML CODE FOR SITE CONVERSION/IS300Fall2022-IS300Portfolio/README.md
+++
@@ -1,6 +0,0 @@
-# IS300Fall2022
-
-Our repo for the portfolio website, a wire-frame to be completed by end of Fall quarter in 2022. :octocat:
-
-- [x] Create Repo
-- [ ] Add Java Script
--- IS302/ORIGINAL HTML CODE FOR SITE CONVERSION/IS300Fall2022-IS300Portfolio/submit.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = setForm;
-function setForm() {
-document.forms[0].onsubmit = function() {
-	if (this.checkValidity()) alert("No invalid data detected. Will retain data
-		for further testing.");
-	return false;
-}
-}
