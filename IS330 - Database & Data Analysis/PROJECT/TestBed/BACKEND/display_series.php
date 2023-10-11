b876f3a994f740c5c1364e5fe52df0f9c3d8c785
flowxyno <tracy.l.harvey6@gmail.com>
2023-10-10 20:48:52 -0700

    added to my code base

---
+++ IS330 - Database & Data Analysis/PROJECT/TestBed/BACKEND/display_series.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <Title>User Selection</Title>
+</head>
+
+<body>
+    <h1>Series Tracker for Selected user:</h1>
+
+    <?php 
+
+    // Connection to the database connection file
+    include "db_connection.php";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+    }
+
+    ?>
+</body>
+
+</html>
