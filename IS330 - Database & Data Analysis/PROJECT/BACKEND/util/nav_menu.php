8c365b810f8ecb695080c83f02d6b31d6d97ce62
flowxyno <tracy.l.harvey6@gmail.com>
2023-12-01 22:11:01 -0800

    cleaning up my school folder

--- IS330 - Database & Data Analysis/PROJECT/BACKEND/util/nav_menu.php
+++
@@ -1,6 +0,0 @@
-<h1>Navigation Menu</h1>
-    <p><a href="index.php?action=show_main_menu">Main Menu</a></p>
-    <p><a href="index.php?action=show_new_user">New User</a></p>
-    <p><a href="index.php?action=show_new_series">New Series</a></p>
-    <p><a href="index.php?action=show_new_episodes">New Episodes</a></p>
-    <p><a href="index.php?action=logout">Logout</a></p>
