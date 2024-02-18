528a0e11eb838374fabd1022f044516e7fe4ccd3
flowxyo <tracy.l.harvey6@gmail.com>
2024-02-18 00:49:49 -0800

    updated homework

--- IS305 - Scripting for Automation/Code/Demos/Mod_7_Demo.py
+++ IS305 - Scripting for Automation/Code/Demos/Mod_7_Demo.py
@@ -1,33 +1,32 @@
 import openpyxl
 
 # Path to the directory containing the data
-dirPath = r'S:\Testing_Ground\SampleData'
+workdir = r'S:\Testing_Ground\SampleData'
 
 # Load the workbook as wb
-wb = openpyxl.load_workbook(dirPath + r'\MTG_Card_Sample.xlsx')
+wb = openpyxl.load_workbook(workdir + r'\MTG_Card_Sample.xlsx')
 
 # Get the active sheet
-orgSheet = wb.active
+origSheet = wb.active
 
 # Create a new workbook to save requested data in
-wb_rare = openpyxl.Workbook()
+rare_wb = openpyxl.Workbook()
 
 # Get the active sheet in the new workbook
-rareSheet = wb_rare.active
+rareSheet = rare_wb.active
 
 # Copy header row for column names
-for col in range(1, orgSheet.max_column +1):
-    rareSheet.cell(row=1, column=col).value = orgSheet.cell(row=1, column=col).value
+for col in range(1, origSheet.max_column +1):
+    rareSheet.cell(row=1, column=col).value = origSheet.cell(row=1, column=col).value
 
 # Copy data for rare cards
-rare_row = 2
-for row in range(2, orgSheet.max_row +1):
-    rarity = orgSheet.cell(row=row, column = 2).value
+rareRow = 2
+for row in range(2, origSheet.max_row +1):
+    rarity = origSheet.cell(row=row, column=2).value
     if rarity == 'Rare':
-        for col in range(1, orgSheet.max_column + 1):
-            rareSheet.cell(row=rare_row, column=col).value = orgSheet.cell(row=row, column=col).value
-
-        rare_row +=1
+        for col in range(1, origSheet.max_column +1):
+            rareSheet.cell(row=rareRow, column=col).value = origSheet.cell(row=row, column=col).value
+        rareRow += 1
 
 # Save the new workbook
-wb_rare.save(dirPath + r'\MTG_Rare_Cards.xlsx')
+rare_wb.save(workdir + r'\MTG_Rare_Cards.xlsx')
